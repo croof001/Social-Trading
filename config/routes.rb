08fd945135f6001d7b3883e6f8345173d744ea12
Vishnuprasad <vishnu@yubi.in>
2014-01-31 15:56:41 +0530

    prevented activeadmin css interference

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Social::Application.routes.draw do
+ 
   devise_for :clients do
-  get 'home', :to => 'dashboard#index', :as => :clients_root
+    get 'home', :to => 'dashboard#index', :as => :clients_root
   end
+  
   resources :tweets
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :keywords
 
-  resources :clients
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
