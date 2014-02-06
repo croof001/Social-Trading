522bff776ac92b29afdaf16a852b99493144cd15
Vishnuprasad <vishnu@yubi.in>
2014-02-06 17:26:35 +0530

    changed message field of tweets from text to string

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140206113357) do
+ActiveRecord::Schema.define(version: 20140206115035) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -105,7 +105,7 @@ ActiveRecord::Schema.define(version: 20140206113357) do
 
   create_table "tweets", force: true do |t|
     t.string   "author"
-    t.text     "message"
+    t.string   "message"
     t.string   "twitter_uuid"
     t.datetime "posted_at"
     t.integer  "client_id"
